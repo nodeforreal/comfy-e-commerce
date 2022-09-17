891389fab6082191a158013dfe6ca26d1383c409
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-17 16:46:01 +0000

    Update CartSection.jsx

--- src/components/CartSection.jsx
+++ src/components/CartSection.jsx
@@ -1,29 +1,50 @@
-import react from "react";
 import styled from "styled-components";
+import ProductQuantity from "./ProductQuantity";
+import { MdDelete } from "react-icons/md";
 
 const CartSection = () => {
   return (
     <Wrapper className="section-center">
-      <table className="table">
-        <tr>
-          <th>item</th>
-          <th>price</th>
-          <th>quantity</th>
-          <th>subtotal</th>
-          <th></th>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-      </table>
+      <div className="table-cart-head">
+        <p>item</p>
+        <p>price</p>
+        <p>quantity</p>
+        <p>subtotal</p>
+        <p></p>
+      </div>
+      <div className="table-cart-content">
+        <div className="item">
+          <img src={""} alt="" />
+          <div>
+            <h5 className="name">title</h5>
+            <p className="color">
+              color : <span></span>
+            </p>
+          </div>
+        </div>
+        <p className="price">$500</p>
+        <div className="quantity">
+          <ProductQuantity />
+        </div>
+        <p className="subtotal">$40000</p>
+        <div className="delete">
+          <button className="cart-delete-btn">
+            <MdDelete />
+          </button>
+        </div>
+      </div>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  margin-top: 2rem;
+  .table-head {
+    text-transform: capitalize;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 0.4fr;
+    justify-items: center;
+  }
+`;
 
 export default CartSection;
