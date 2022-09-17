765b97551dbbcde4212ccd258daa0a7a9218ec7f
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-17 16:51:26 +0000

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,28 +1,14 @@
-import React from "react";
-import { CartEmpty, CartSection, PageHero } from "../components";
-import styled from "styled-components/macro";
+import React from 'react'
+import { CartEmpty } from '../components';
+import styled from 'styled-components/macro';
 
 const Cart = () => {
-  if (false) {
-    // return (
-    //   <Wrapper className="page-100">
-    //     <CartEmpty
-    //       css={`
-    //         padding-top: 2rem;
-    //       `}
-    //     />
-    //   </Wrapper>
-    // );
-  }
-
   return (
-    <Wrapper className="page-100">
-      <PageHero title="cart" />
-      <CartSection />
+    <Wrapper className='page-100'>
+      <CartEmpty css={`padding-top: 2rem;`}/>
     </Wrapper>
-  );
-};
+  )
+}
 
-const Wrapper = styled.section``;
-
-export default Cart;
+const Wrapper = styled.section``
+export default Cart
