b3f91588cc729390fce5f708982fa15cca30d150
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-18 10:02:52 +0000

    Create FlatButton.jsx

---
+++ src/components/FlatButton.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Wrapper = styled.button`
+  padding: 0.25rem 0.5rem;
+  text-transform: capitalize;
+  background-color: var(--clr-red-dark);
+  border-radius: var(--radius);
+  font-size: 0.9rem;
+  color: var(--clr-white);
+  letter-spacing: var(--spacing);
+`;
+
+export default Wrapper;
