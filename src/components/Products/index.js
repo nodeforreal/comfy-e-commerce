bb38d6d1605a4eb17094374d3aa1738943ed375b
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-18 15:16:38 +0000

    Update index.js

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -2,5 +2,6 @@ import ViewButtons from "./ViewButtons";
 import SortBy from "./SortBy";
 import ProductItem from "./ProductItem";
 import ViewBar from "./ViewBar";
+import NoProducts from "./NoProducts";
 
-export { ViewButtons, SortBy, ProductItem, ViewBar };
+export { ViewButtons, SortBy, ProductItem, ViewBar, NoProducts };
