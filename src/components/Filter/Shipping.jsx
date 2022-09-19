67165eb571d7b914bbd73039d61b6728c9b04f89
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-19 15:38:48 +0000

    Update Shipping.jsx

--- src/components/Filter/Shipping.jsx
+++ src/components/Filter/Shipping.jsx
@@ -12,7 +12,6 @@ const Shipping = () => {
       <input
         id="shipping-filter"
         type="checkbox"
-        checked={isFreeShipping}
         onChange={() => setFreeShipping()}
       />
     </Wrapper>
