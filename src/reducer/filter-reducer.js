d0d8b4769572d0737e6b8d8912fec0f11d5e4e18
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-19 08:24:08 +0000

    Update filter-reducer.js

--- src/reducer/filter-reducer.js
+++ src/reducer/filter-reducer.js
@@ -19,8 +19,8 @@ const filterReducer = (state, { type, payload }) => {
   switch (type) {
     case FILTER_BEGIN:
       return { ...state, filter_begin: payload };
-       case SET_FILTER_LIST:
-      return { ...state, filter_list: payload };
+    case SET_FILTER_LIST:
+      return { ...state, filter: payload };
     case SET_PRODUCTS:
       return { ...state, products: payload, filtered_products: payload };
 
