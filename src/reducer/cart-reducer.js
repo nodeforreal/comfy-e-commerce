2815b55d1641255d05400425d830ec652a509cec
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-21 12:07:37 +0000

    Update cart-reducer.js

--- src/reducer/cart-reducer.js
+++ src/reducer/cart-reducer.js
@@ -3,7 +3,24 @@ import { ADD_TO_CART } from "../actions";
 // CART REDUCER
 const cartReducer = (state, { type, payload }) => {
   if (type === ADD_TO_CART) {
-    return { ...state, cart_items: [...state.cart_items, payload] };
+    if (state.cart_items.length === 0) {
+      return { ...state, cart_items: [payload] };
+    }
+    //for update quality, subtotal.
+    let tempCart = []
+    const cartItems = state.cart_items.map((cartItem) => {
+      if (cartItem.id !== payload.id) return cartItem;
+      if(cartItem.color !== payload.color){
+          tempCart.push(payload)
+      }
+      
+      let selectedQuantity, subTotal;
+      selectedQuantity = cartItem.selectedQuantity + payload.selectedQuantity;
+      subTotal = cartItem.price * selectedQuantity;
+      return { ...cartItem, selectedQuantity, subTotal };
+    });
+
+    return { ...state, cart_items: cartItems };
   }
 
   throw new Error("Action type - mis-match. " + type);
