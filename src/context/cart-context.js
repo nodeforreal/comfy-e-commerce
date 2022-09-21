0887d81cdce9d03354486709e452deb85f4de968
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-21 14:15:36 +0000

    Update cart-context.js

--- src/context/cart-context.js
+++ src/context/cart-context.js
@@ -1,6 +1,12 @@
 import { createContext, useReducer, useContext } from "react";
 import { cartReducer } from "../reducer/cart-reducer";
-import { ADD_TO_CART } from "../actions";
+import {
+  ADD_TO_CART,
+  ADD_ITEM_QUANTITY,
+  REMOVE_ITEM_QUANTITY,
+  REMOVE_CART_ITEM,
+  CLEAR_CART,
+} from "../actions";
 
 const CartContext = createContext({});
 
@@ -13,12 +19,42 @@ const CartContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, initialState);
 
   const addToCart = (cartItem) => {
-
-    dispatch({ type: ADD_TO_CART, payload: cartItem });
+    let { id, selectedColor } = cartItem;
+    // Unique item id.
+    let itemId = id + selectedColor;
+    dispatch({
+      type: ADD_TO_CART,
+      payload: { ...cartItem, itemId },
+    });
   };
 
+  const addItemQuantity = (itemId) => {
+    dispatch({ type: ADD_ITEM_QUANTITY, payload: itemId });
+  };
+  
+  const removeItemQuantity = (itemId) => {
+    dispatch({ type: REMOVE_ITEM_QUANTITY, payload: itemId });
+  };
+  
+  const removeCartItem = (itemId) => {
+    dispatch({ type: REMOVE_CART_ITEM, payload: itemId });
+  };
+  
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART, payload: initialState });
+  };
+  
   return (
-    <CartContext.Provider value={{ ...state, addToCart }}>
+    <CartContext.Provider
+      value={{
+        ...state,
+        addToCart,
+        addItemQuantity,
+        removeItemQuantity,
+        removeCartItem,
+        clearCart,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
