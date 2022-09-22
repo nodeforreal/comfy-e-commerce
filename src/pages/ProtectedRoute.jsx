e3daa58b487cf5efde4b952f4b5bdc6ec80ea506
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-22 12:51:25 +0000

    Update ProtectedRoute.jsx

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,5 +1,12 @@
-const ProtectedRoute = () => {
-  return null;
+import { useAuth0 } from "@auth0/auth0-react";
+
+const ProtectedRoute = ({ children }) => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <h1>Loading....</h1>;
+  }
+  return <>{children}</>;
 };
 
 export default ProtectedRoute;
