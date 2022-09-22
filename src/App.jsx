082568e0b3bfeb2857a1c7529966c6c12d95dfa5
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-22 13:06:31 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home, About, Products, Cart, Checkout, SingleProduct, Error } from "./pages";
-import { NavBar, SideBar , Footer} from "./components";
+import {
+  Home,
+  About,
+  Products,
+  Cart,
+  Checkout,
+  SingleProduct,
+  Error,
+  ProtectedRoute,
+} from "./pages";
+import { NavBar, SideBar, Footer } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <NavBar />
-      <SideBar />
-      <Routes>
-        <Route path="/" exact element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/products" element={<Products />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-        <Route path="/checkout" element={<Checkout />}></Route>
-        <Route path="/products/:productId" element={<SingleProduct />}></Route>
-        <Route path="*" element={<Error />}></Route>
-      </Routes>
-      <Footer />
+      <ProtectedRoute>
+        <NavBar />
+        <SideBar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/products" element={<Products />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/checkout" element={<Checkout />}></Route>
+          <Route
+            path="/products/:productId"
+            element={<SingleProduct />}
+          ></Route>
+          <Route path="*" element={<Error />}></Route>
+        </Routes>
+        <Footer />
+      </ProtectedRoute>
     </BrowserRouter>
   );
 };
