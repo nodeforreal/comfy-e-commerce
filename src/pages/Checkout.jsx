86d73792ed60ff8c9b22f9b5890404b69d5e48fb
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-23 07:22:49 +0000

    Update Checkout.jsx

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -3,10 +3,12 @@ import styled from "styled-components";
 import { CartEmpty, PageHero } from "../components";
 import { useCartContext } from "../context/cart-context";
 import { useUserContext } from "../context/user-context";
+import { formatPrice } from "../utils/currency";
 
 const Checkout = () => {
   const { user } = useUserContext();
-  const { cart_items } = useCartContext();
+  const { cart_items, order_total } = useCartContext();
+
   if (cart_items.length === 0) {
     return (
       <Wrapper className="page-100">
@@ -18,12 +20,11 @@ const Checkout = () => {
   return (
     <Wrapper className="page-100">
       <PageHero title="Checkout" />
-      
-      <div>
-          <h4>Hello, {user.name}</h4>
-          <p>Your total is {}</p>
 
-</div>
+      <div>
+        <h4>Hello, {user.nickname}</h4>
+        <p>Your total is {formatPrice(order_total)}</p>
+      </div>
     </Wrapper>
   );
 };
