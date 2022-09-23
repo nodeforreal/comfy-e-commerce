9a40b1b4e2c716bb28152d2c788e870d26e31c6f
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-23 05:33:34 +0000

    Update AuthRoute.jsx

--- src/pages/AuthRoute.jsx
+++ src/pages/AuthRoute.jsx
@@ -3,15 +3,15 @@ import {useUserContext} from "../context/user-context";
 const AuthRoute = ({ children }) => {
     const {isLoading, isError} = useUserContext()
     
-  if (isLoading) {
-    return <h1>Loading....</h1>;
-  }
-  
-  if (isError) {
-    return <h1>Error....</h1>;
-  }
-  
-  return <>{children}</>;
+    if (isLoading) {
+        return <h1>Loading....</h1>;
+    }
+    
+    if (isError) {
+        return <h1>Error....</h1>;
+    }
+    
+    return <>{children}</>;
 };
 
 export default AuthRoute;
