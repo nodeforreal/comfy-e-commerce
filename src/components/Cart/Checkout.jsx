d1d76016e33c812fe32ebe8380694593a4e14385
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-23 07:02:19 +0000

    Update Checkout.jsx

--- src/components/Cart/Checkout.jsx
+++ src/components/Cart/Checkout.jsx
@@ -1,33 +1,30 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../context/cart-context";
-import { cartCheckout } from "../../utils/cart-checkout";
 import { useUserContext } from "../../context/user-context";
 import { useAuth0 } from "@auth0/auth0-react";
+import { formatPrice } from "../../utils/currency";
 
 const Checkout = () => {
-  const { cart_items } = useCartContext();
+  const { sub_total, shipping_fee, order_total } = useCartContext();
   const { isAuthenticated } = useUserContext();
   const { loginWithRedirect } = useAuth0();
-
-  const cart_checkout = cartCheckout(cart_items, 55.5);
-  const { sub_total, shipping_fee, order_total } = cart_checkout;
 
   return (
     <Wrapper>
       <div className="checkout-card">
         <div className="subtotal flex-even">
           <h5>subtotal :</h5>
-          <h5>{sub_total}</h5>
+          <h5>{formatPrice(sub_total)}</h5>
         </div>
         <div className="shipping-fee flex-even">
           <p>Shipping Fee :</p>
-          <p>{shipping_fee}</p>
+          <p>{formatPrice(shipping_fee)}</p>
         </div>
         <hr />
         <div className="order-total flex-even">
           <h4>Order Total :</h4>
-          <h4>{order_total}</h4>
+          <h4>{formatPrice(order_total)}</h4>
         </div>
       </div>
       {isAuthenticated ? (
@@ -35,10 +32,7 @@ const Checkout = () => {
           proceed to checkout
         </Link>
       ) : (
-        <button
-          className="btn login-btn"
-          onClick={loginWithRedirect}
-        >
+        <button className="btn login-btn" onClick={loginWithRedirect}>
           Login
         </button>
       )}
