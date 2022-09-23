5fcedfd92c6240763098c0842c5bce0139f325cc
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-23 07:02:05 +0000

    Update cart-checkout.js

--- src/utils/cart-checkout.js
+++ src/utils/cart-checkout.js
@@ -1,12 +1,9 @@
-import { formatPrice } from "./currency";
-
 const cartCheckout = (cartItems, shippingFee) => {
   let sub_total, order_total, shipping_fee;
   sub_total = cartItems.reduce((total, { subTotal }) => total + subTotal, 0);
 
-  order_total = formatPrice(sub_total + shippingFee);
-  shipping_fee = formatPrice(shippingFee);
-  sub_total = formatPrice(sub_total);
+  order_total = sub_total + shippingFee;
+  shipping_fee = shippingFee;
 
   return {
     sub_total,
