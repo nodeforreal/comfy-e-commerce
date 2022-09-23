f94d00cfb292c99bee18d1dd0c53e40d3a68ec23
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-23 07:02:12 +0000

    Update Checkout.jsx

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import styled from "styled-components";
-import { CartEmpty,PageHero } from "../components";
+import { CartEmpty, PageHero } from "../components";
+import { useCartContext } from "../context/cart-context";
+import { useUserContext } from "../context/user-context";
 
 const Checkout = () => {
-  return <Wrapper className="page-100">
-    <PageHero title ="Checkout" />
-    <CartEmpty />
-  </Wrapper>;
+  const { user } = useUserContext();
+  const { cart_items } = useCartContext();
+  if (cart_items.length === 0) {
+    return (
+      <Wrapper className="page-100">
+        <PageHero title="Checkout" />
+        <CartEmpty />
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper className="page-100">
+      <PageHero title="Checkout" />
+      
+      <div>
+          <h4>Hello, {user.name}</h4>
+          <p>Your total is {}</p>
+
+</div>
+    </Wrapper>
+  );
 };
 
-const Wrapper = styled.section``
+const Wrapper = styled.section``;
 export default Checkout;
