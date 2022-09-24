d38002cba36415b94624b4208ec6537711931e46
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-24 12:51:24 +0000

    Update StripeCheckout.jsx

--- src/components/StripeCheckout.jsx
+++ src/components/StripeCheckout.jsx
@@ -50,7 +50,7 @@ export default function StripeCheckout() {
   );
 }
 
-function CheckoutForm() {
+function CheckoutForm({ clientSecret }) {
   const stripe = useStripe();
   const elements = useElements();
 
@@ -61,10 +61,6 @@ function CheckoutForm() {
     if (!stripe) {
       return;
     }
-
-    const clientSecret = new URLSearchParams(window.location.search).get(
-      "payment_intent_client_secret"
-    );
 
     console.log(clientSecret);
 
