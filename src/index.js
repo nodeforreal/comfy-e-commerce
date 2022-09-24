634f9cd0166d01762ddc93c63bc5250edd6f87bc
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-24 04:10:46 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ const root = reactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Auth0Provider
-      domain={process.env.AUTH0_DOMAIN}
-      clientId={process.env.AUTH0_CLIENT_ID}
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
       redirectUri={window.location.origin}
       cacheLocation="localstorage"
     >
