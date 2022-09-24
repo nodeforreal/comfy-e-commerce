1d87bd664ecfa7dd783fc7a17006c3d6f7b92df3
keeforever <kaliyamoorthy.2k@gmail.com>
2022-09-24 12:30:33 +0000

    Update StripeCheckout.jsx

--- src/components/StripeCheckout.jsx
+++ src/components/StripeCheckout.jsx
@@ -103,7 +103,7 @@ function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "https://comfy-store-active.netlify.app/",
+        return_url: "https://comfy-store-active.netlify.app/checkout",
       },
     });
 
